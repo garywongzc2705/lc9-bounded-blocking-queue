0e64dcb946732b93aa5a186aeff60813dde5f39d
Gary Wong <garywongzc2705@gmail.com>
2026-03-28 16:41:53 -0400

    Add spinlock example solution

---
+++ level1_solution1.py
@@ -0,0 +1,38 @@
+from collections import deque
+import threading
+import time
+
+
+# correct implementation with spin lock
+# sleep causes CPU usage, the longer you wait, you are not
+# reactive enough and have latency bug (and fairness), the shorter
+# you wait, you increase significant CPU overhead
+class BoundedBlockingQueue:
+    def __init__(self, capacity: int) -> None:
+        self.capacity = capacity
+        self.q = deque()
+        self.lock = threading.Lock()
+
+    def put(self, item):
+        while True:
+            with self.lock:
+                if len(self.q) < self.capacity:
+                    self.q.append(item)
+                    return
+            time.sleep(0.2)
+
+    def take(self):
+        while True:
+            with self.lock:
+                if len(self.q) > 0:
+                    return self.q.popleft()
+            time.sleep(0.2)
+
+    def size(self):
+        return len(self.q)
+
+    def is_empty(self):
+        return len(self.q) == 0
+
+    def is_full(self):
+        return len(self.q) == self.capacity

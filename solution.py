b21cde4fb1b8dd1b722c59c97f300c12275b3c21
Gary Wong <garywongzc2705@gmail.com>
2026-03-28 15:41:31 -0400

    Add level 1 solution

--- solution.py
+++ solution.py
@@ -1,3 +1,7 @@
+from collections import deque
+import threading
+import time
+
 # Anthropic — Staff Engineer · Incremental Coding Round #10
 # solution.py — Bounded Blocking Queue
 #
@@ -13,4 +17,31 @@
 
 
 class BoundedBlockingQueue:
-    pass
+    def __init__(self, capacity: int) -> None:
+        self.capacity = capacity
+        self.q = deque()
+        self.lock = threading.Lock()
+
+    def put(self, item):
+        while True:
+            with self.lock:
+                if len(self.q) < self.capacity:
+                    self.q.append(item)
+                    return
+            time.sleep(0.2)
+
+    def take(self):
+        while True:
+            with self.lock:
+                if len(self.q) > 0:
+                    return self.q.popleft()
+            time.sleep(0.2)
+
+    def size(self):
+        return len(self.q)
+
+    def is_empty(self):
+        return len(self.q) == 0
+
+    def is_full(self):
+        return len(self.q) == self.capacity
